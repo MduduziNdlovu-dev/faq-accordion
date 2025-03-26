cb51266f710063455a4039fedfbec1fabd505390
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-03-26 19:58:42 +0200

    Feat: Implement FAQ section with dynamic QuestionAccordion components and global styles

--- src/components/FAQContainer.tsx
+++ src/components/FAQContainer.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import { Typography } from '@mui/material'
+import data from '../../data/data.json'
+import QuestionAccordion from './QuestionAccordion'
 
 const FAQContainer = () => {
   return (
-    <div>FAQContainer</div>
+    <Box
+        sx={{
+            backgroundColor: "#FFF",
+            padding: "2rem",
+            borderRadius: "2rem"
+        }}
+    >
+        <Typography
+            variant='h1'
+        >
+            FAQs
+        </Typography>
+        {data.faqs.map((qA)=>{
+            return <QuestionAccordion questionAnswer={qA} key={qA.answer}/>
+        })}
+    </Box>
   )
 }
 
--- src/components/QuestionAccordion.tsx
+++ src/components/QuestionAccordion.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+'use client'
+import { Box, Button, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
-const QuestionAccordion = () => {
+interface Props {
+    questionAnswer: {
+        question: string,
+        answer: string
+    }
+}
+
+const QuestionAccordion = ({questionAnswer}: Props) => {
+    const [showAnswer, setShowAnswer] = useState(false);
+    const {question, answer} = questionAnswer;
   return (
-    <div>QuestionAccordion</div>
+    <Box>
+        <Box
+            sx={{
+                display:"flex",
+                justifyContent:"space-between",
+                alignItems:"center"
+            }}
+        >
+            <Typography>
+                {question}
+            </Typography>
+            <Button onClick={() => setShowAnswer(!showAnswer)}>+</Button>
+        </Box>
+
+        <Box sx={{width:"32.5rem", display: showAnswer ? "block" : "none"}}>
+            <Typography>
+                {answer}
+            </Typography>
+
+        </Box>
+        
+    </Box>
   )
 }
 
